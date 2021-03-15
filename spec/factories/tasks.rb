2575d5678844b627ba1071686bc7373bc68d7e92
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-03-15 11:13:16 +0900

    02_model spec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    title {"wasshoi"}
+    content {"hogehoge"}
+    status {1}
+    user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it '全ての属性が有効であること' do
+      expect(FactoryBot.build(:task)).to be_valid
+    end
+
+    it 'タイトルがなければ無効であること' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'ステータスがなければ無効であること' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'タイトルが重複していれば無効であること' do
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task)
+      task.valid?
+      expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it '別のタイトルであれば有効であること' do
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task, title: "hogehoge")
+      task.valid?
+      expect(task).to be_valid
+    end
+  end
 end
